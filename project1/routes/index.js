df5aa9d905828ce245da53bf31886fd9109e247e
sanjana yelligaddala <sanjanayelligaddala@gmail.com>
2025-08-19 19:37:29 +0530

    first commit

--- project1/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'stock-trader' });
-});
-
-module.exports = router;
